a17e0a6d89228125a6a1774c1622e5750b6a93b9
jhonatan-pires <jhonatanpires636@gmail.com>
2020-10-01 21:05:07 -0300

    Initial commit

---
+++ src/main/java/sistema/api/desenvolvimento/api/gerencimento/ApiGerencimentoApplication.java
@@ -0,0 +1,13 @@
+package sistema.api.desenvolvimento.api.gerencimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerencimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerencimentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sistema/api/desenvolvimento/api/gerencimento/ApiGerencimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema.api.desenvolvimento.api.gerencimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerencimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
