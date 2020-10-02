3c9b188275b605b438f4f80c8a5dc74a80c37ef6
jhonatan-pires <jhonatanpires636@gmail.com>
2020-10-01 21:28:13 -0300

    criado um return para o API Test

--- src/main/java/sistema/api/desenvolvimento/api/gerencimento/ApiGerencimentoApplication.java
+++ src/main/java/sistema/api/desenvolvimento/api/gerencimento/ApiGerencimentoApplication.java
@@ -2,10 +2,19 @@ package sistema.api.desenvolvimento.api.gerencimento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1/people")
 @SpringBootApplication
 public class ApiGerencimentoApplication {
 
+    @GetMapping
+    public String getBook(){
+        return "API Test!";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGerencimentoApplication.class, args);
 	}
